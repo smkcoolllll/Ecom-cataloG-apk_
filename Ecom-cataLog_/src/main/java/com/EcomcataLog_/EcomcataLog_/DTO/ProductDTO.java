b52c99d35df21b01e8ba21de5e1e74999e0ea47f
SACHIN KOKARE <62193791+smkcoolllll@users.noreply.github.com>
2023-11-28 14:36:38 +0530

    Update ProductDTO.java

--- Ecom-cataLog_/src/main/java/com/EcomcataLog_/EcomcataLog_/DTO/ProductDTO.java
+++ Ecom-cataLog_/src/main/java/com/EcomcataLog_/EcomcataLog_/DTO/ProductDTO.java
@@ -7,6 +7,6 @@ public class ProductDTO {
     private Long id;
     private String name;
     private double price;
-    private Long categoryId; // Use categoryId to link the product to a category
+    private Long categoryId; 
     private String productDesc;
 }
